c135fbe48db293e703bb9ce48843961f6d86528a
Rodrigo <rocostaparis@hotmail.com>
2023-06-13 20:02:26 -0300

    feat: created relationship between Category and Post entity

--- Entities/Post.cs
+++ Entities/Post.cs
@@ -15,6 +15,9 @@ namespace Blog.Entities
         public int UserId { get; set; }
         public User User { get; set; }
 
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
 
     }
 }
